dfb50b75c51c4ad5d7d52c57d4464000ed5856e8
Fabrício Varisco Oliveira <fabriciooliveira@ciandt.com>
2023-12-28 11:12:09 -0300

    first

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace razor_poc;
+
+public class Program
+{
+    public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureCmsDefaults()
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+}
